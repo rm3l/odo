4e53b7fb592b3ffadfe273db32726f994a078445
Armel Soro <asoro@redhat.com>
2023-04-07 17:44:34 +0200

    Fix release-bit-verification.sh script used by QE to verify redistributable RPMs
    - Validate script argument
    - Allow to run the script on more platforms
    - Enhance version checks by checking Git commit IDs as well, to make sure the right version of odo was included in the RPM
    - Fix Make targets: run only E2E tests against our internal and stable Devfile registry

--- scripts/release-bit-verification.sh
+++ scripts/release-bit-verification.sh
@@ -1,10 +1,14 @@
-#!/usr/bin/sh
+#!/bin/bash
+
+set -euo pipefail
 
 ############################################################################
 #  PREREQUISITES FOR THIS SCRIPT
+# 0. Download redistributable RPM file from the Errata Builds tab
 # 1. Redistributable-binary(.rpm) should be passed as the first argument
 # 2. Login to the cluster should be done prior to running this script
 # 3. The cluster should be in a state where it can be used for testing
+#   3.1. For example, you might need to run ./scripts/configure-cluster/common/setup-operators.sh to install the Operators (like SBO) for the tests
 #
 # USAGE:
 # ./release-bit-verification.sh redistributable-binary
@@ -13,24 +17,49 @@
 #
 
 shout() {
-    echo "--------------------------------$1------------------------------------------"
+  echo
+  echo "--------------------------------${1:-}------------------------------------------"
+  echo
 }
+
+uname_os() {
+  os=$(uname -s | tr '[:upper:]' '[:lower:]')
+  case "$os" in
+  msys* | cygwin* | mingw*)
+    os='windows'
+    ;;
+  esac
+  echo "$os"
+}
+
+uname_arch() {
+  arch=$(uname -m)
+  case $arch in
+  x86_64) arch="amd64" ;;
+  aarch64) arch="arm64" ;;
+  esac
+  echo "$arch"
+}
+
+RPM="${1:-}"
+[ -n "${RPM}" ] || (echo "Missing path to the redistributable RPM to verify. You can download it from the Errata Builds tab."; exit 1)
+
 # Check SHASUM for all the binary files and there should be no difference
 
 # Create a Temp directory
-WORKING_DIR=$(mktemp -d)
+WORKING_DIR="$(mktemp -d)"
 shout "WORKING_DIR=$WORKING_DIR"
 export REPO_URL=${REPO_URL:-"https://github.com/redhat-developer/odo.git"}
 
 # Extract from rpm file
-rpm2cpio ${1} | cpio -idmvD $WORKING_DIR
-pushd $WORKING_DIR/usr/share/odo-redistributable/
+rpm2cpio "$RPM" | cpio -idmvD "$WORKING_DIR"
+pushd "$WORKING_DIR"/usr/share/odo-redistributable/
 
 # Check sha256sum for all the files
 while IFS= read -r line; do
     read -r SHA FILE <<<"$line"
-    read -r SHATOCHECK FILE <<<$(sha256sum $FILE)
-    if [[ $SHA == $SHATOCHECK ]]; then
+    read -r SHATOCHECK FILE <<<"$(sha256sum "$FILE")"
+    if [[ $SHA == "$SHATOCHECK" ]]; then
         # Print if the file is correct
         printf '%-50s\U0002705\n' $FILE
     fi
@@ -38,39 +67,55 @@ done <SHA256_SUM
 
 shout
 
-# Copy binary for testing purpose
-OS=$(uname -s)
-ARCH=$(uname -m)
-
-if [[ $OS == "Linux" ]]; then
-    if [[ $ARCH == "x86_64" ]]; then
-        cp ./odo-linux-amd64 odo
-        PATH=$(pwd):$PATH
-    fi
+os="$(uname_os)"
+if [[ "$os" == "windows" ]]; then
+  echo "Not supported on Windows"
+  exit 1
 fi
+arch="$(uname_arch)"
+ODO_BINARY="$(pwd)/odo-${os}-${arch}"
+[ -f "$ODO_BINARY" ] || (echo "Binary $ODO_BINARY not found for platform $os $arch" && exit 1)
+# Copy binary for testing purposes
+cp "$ODO_BINARY" "$(pwd)/odo"
+PATH="$(pwd):$PATH"
+export PATH
 
-# Check odo verion and if it is correct
+# Use a dedicated preferences file
+export GLOBALODOCONFIG="$WORKING_DIR/preferences.yaml"
+export ODO_TRACKING_CONSENT="no"
+
+# Use a stable odo registry for testing
+export DEVFILE_REGISTRY="https://devfile-registry-ci-devfile-registry.odo-test-kubernetes-clust-49529fc6e6a4a9fe7ebba9a3db5b55c4-0000.eu-de.containers.appdomain.cloud"
+
+shout "Checking version"
+odo version
+
+# Check odo version and if it is correct
 VERSION=$(cat VERSION)
-ODOVERSIONCHECK=$(odo version)
-if [[ "$ODOVERSIONCHECK" == *"$VERSION"* ]]; then
-    echo "odo binary is installed correctly"
-else 
-    echo "odo binary is not installed correctly"
-    exit 1
-fi
-
-#clone repo for testing and checkout release tag
+# Clone repo for testing and checkout release tag.
+# Needed to check the Git commit ID and run the tests.
 pushd $WORKING_DIR
 if [ -d "odo" ]; then
     rm -rf odo
 fi
-git clone $REPO_URL odo && cd $WORKING_DIR/odo && git checkout "v$VERSION"
+git -c advice.detachedHead=false clone --quiet --depth=1 -b "v$VERSION" "$REPO_URL" odo
+pushd "$WORKING_DIR/odo"
 
-#Run tests
-make test-integration-devfile
-make test-integration
-make test-e2e-all
-make test-cmd-project
+ODOVERSIONCHECK=$(odo version --client)
+GIT_SHA_SHORT="$(git rev-parse --short HEAD)"
+EXPECTED_VERSION_WITH_SHA="v$VERSION ($GIT_SHA_SHORT"
+if [[ "${ODOVERSIONCHECK#"odo "}" == "$EXPECTED_VERSION_WITH_SHA"* ]]; then
+    echo "odo binary is installed correctly"
+else
+    echo "Version mismatch. Got ${ODOVERSIONCHECK#"odo "} from the redistributable RPM, but expected $EXPECTED_VERSION_WITH_SHA."
+    exit 1
+fi
+
+# Run only E2E tests
+make test-e2e
 
 # Cleanup
-rm -rf /tmp/odo /tmp/usr
+popd
+popd
+popd
+rm -rf "$WORKING_DIR"
