31296e69246f91ca70444e1c5a7e34158dd679db
Armel Soro <asoro@redhat.com>
2023-06-06 17:40:23 +0200

    wip: some changes to the website

--- docs/website/docusaurus.config.js
+++ docs/website/docusaurus.config.js
@@ -32,7 +32,7 @@ module.exports = {
     },
     announcementBar: {
       id: 'announcementBar-2', // Increment on change
-      content: `⭐️ Love odo? Support us by giving it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/redhat-developer/odo">GitHub</a>! ⭐️`,
+      content: `⭐️ Hey there!!! Love odo? Support us by giving it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/redhat-developer/odo">GitHub</a>! ⭐️`,
     },
     navbar: {
       title: 'odo',
